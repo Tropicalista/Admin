08e7f05b20cd56f4a80d9b9c2cece4642e74b3a6
Tropicalista <francescopepe@email.it>
2018-02-04 17:19:19 +0100

    add select

--- resources/views/settings/partials/common.blade.php
+++ resources/views/settings/partials/common.blade.php
@@ -5,6 +5,11 @@
 			<div class="col-sm-10">
 				@if($setting->type == 'textarea')
 				<textarea class="form-control" name="settings[{{$setting->handle}}]">{{$setting->value}}</textarea>
+				@elseif($setting->type == 'select')
+				<select class="form-control" name="settings[{{$setting->handle}}]">
+					<option value="1">Yes</option>
+					<option value="0">No</option>
+				</select>
 				@else
 				<input class="form-control" type="{{is_numeric($setting->value) ? 'number' : 'text'}}" name="settings[{{$setting->handle}}]" value="{{$setting->value}}" placeholder="{{$setting->default}}">
 				@endif
